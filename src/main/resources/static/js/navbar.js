cab18e5aed32cdc82b5b8b3fb7998b5ed48255c0
Darío Abenza Morales <dario.abenza@um.es>
2020-09-02 13:24:13 +0200

    Navbar dinámico con info de usuario; botón logout

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,15 +1,20 @@
-var autenticado = true
-
-var horas = 450
 var avatar = "media/avatar/piratecat.png"
-var nombre = "dario"
+
 
 $(document).ready(function(){
+    var usuario = JSON.parse(localStorage.getItem("usuario"))
+    var autenticado = usuario !=null
     if(autenticado){
         $("#login").remove()
         $("#registro").remove()
         $("#usuario-nav")
             .append("<img class=\"border rounded-circle img-profile\"src=\""+avatar+"\"></img>\
-            <strong>"+nombre+"</strong> "+horas+"h")
+            <strong>"+usuario.nombre+"</strong> "+usuario.horasTrabajadas+"h\
+            <button id=\"logout\" class=\"btn action-button\" role=\"button\"\">Cerrar sesión</button>")
     }
+    $("#logout").click(function(){
+        window.localStorage.setItem("usuario",null)
+        window.localStorage.setItem("jwt", null)
+        window.location.href="index.html"
+    })
 })
